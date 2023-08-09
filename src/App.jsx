bba2071aff7f5b7b8d4eba800bca77eb520a8abe
Loïc Calcagno <calcagnoloic93@gmail.com>
2023-08-09 14:03:50 +0200

    add functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,67 @@
 import { useState } from "react";
 // eslint-disable-next-line no-unused-vars
 import { Button } from "./components/Button";
-// eslint-disable-next-line no-unused-vars
-import { Footer } from "./components/Footer";
 
 export const App = () => {
-    // eslint-disable-next-line no-unused-vars
-    const [result, setResult] = useState(0);
+    const [expression, setExpression] = useState([]);
+    const [display, setDisplay] = useState("");
+
+    const handleClick = (value) => {
+        setDisplay(value);
+        setExpression([...expression, value]);
+    };
+
+    const remove = () => {
+        if (expression === "") {
+            return;
+        } else {
+            setExpression(expression.slice(0, -1));
+        }
+    };
+
+    const removeAll = () => {
+        if (expression === "") {
+            return;
+        } else {
+            setDisplay("");
+            setExpression("");
+        }
+    };
+
+    const handleResult = () => {
+        const displayResult = expression
+            .join("")
+            .split(/(\D)/g)
+            .map((value) => (value.match(/\d/g) ? parseInt(value, 0) : value))
+
+            .reduce((acc, value, index, array) => {
+                switch (value) {
+                    case "+":
+                        return (acc = acc + array[index + 1]);
+                    case "-":
+                        return (acc = acc - array[index + 1]);
+                    case "*":
+                        return (acc = acc * array[index + 1]);
+                    case "/":
+                        return (acc = acc / array[index + 1]);
+                    default:
+                        return acc;
+                }
+            });
+
+        setExpression("");
+        setDisplay(displayResult);
+    };
 
     return (
         <>
             <div className="max-w-sm m-auto mt-12 sm:mt-24">
-                <div className="bg-slate-500 max-w-md max-h max-h-screen p-5 rounded-sm mx-1 sm:mx-0">
-                    <button className="bg-gray-800 text-white py-5 px-12 rounded-3xl text-end font-bold text-2xl mb-5 w-full">
-                        {result}
+                <div className="bg-slate-500 max-w-md max-h max-h-screen p-5 rounded-2xl mx-3 sm:mx-0">
+                    <button className="bg-gray-800 text-white py-5 px-12 text-end rounded-sm font-bold text-2xl w-full">
+                        {expression}
+                    </button>
+                    <button className="bg-gray-800 text-white py-5 px-12 text-end rounded-sm font-bold text-2xl mb-5 w-full">
+                        Result: {display}
                     </button>
                     <div className="bg-gray-600 rounded-sm p-5">
                         <div className="flex justify-around mb-2">
@@ -22,24 +70,28 @@ export const App = () => {
                                 design={
                                     "bg-gray-200 border-b-gray-400 text-slate-800 h-10 w-10 sm:w-16"
                                 }
+                                handler={() => handleClick(7)}
                             />
                             <Button
                                 text="8"
                                 design={
                                     "bg-gray-200 border-b-gray-400 text-slate-800 h-10 w-10 sm:w-16"
                                 }
+                                handler={() => handleClick(8)}
                             />
                             <Button
                                 text="9"
                                 design={
                                     "bg-gray-200 border-b-gray-400 text-slate-800 h-10 w-10 sm:w-16"
                                 }
+                                handler={() => handleClick(9)}
                             />
                             <Button
                                 text="DEL"
                                 design={
                                     "bg-indigo-500 border-b-indigo-700 text-white h-10 w-10 sm:w-16 text-xl"
                                 }
+                                handler={() => remove()}
                             />
                         </div>
                         <div className="flex justify-around mb-2">
@@ -48,24 +100,28 @@ export const App = () => {
                                 design={
                                     "bg-gray-200 border-b-gray-400 text-slate-800 h-10 w-10 sm:w-16"
                                 }
+                                handler={() => handleClick(4)}
                             />
                             <Button
                                 text="5"
                                 design={
                                     "bg-gray-200 border-b-gray-400 text-slate-800 h-10 w-10 sm:w-16"
                                 }
+                                handler={() => handleClick(5)}
                             />
                             <Button
                                 text="6"
                                 design={
                                     "bg-gray-200 border-b-gray-400 text-slate-800 h-10 w-10 sm:w-16"
                                 }
+                                handler={() => handleClick(6)}
                             />
                             <Button
                                 text="+"
                                 design={
                                     "bg-gray-200 border-b-gray-400 text-slate-800 h-10 w-10 sm:w-16"
                                 }
+                                handler={() => handleClick("+")}
                             />
                         </div>
                         <div className="flex justify-around mb-2">
@@ -74,24 +130,28 @@ export const App = () => {
                                 design={
                                     "bg-gray-200 border-b-gray-400 text-slate-800 h-10 w-10 sm:w-16"
                                 }
+                                handler={() => handleClick(1)}
                             />
                             <Button
                                 text="2"
                                 design={
                                     "bg-gray-200 border-b-gray-400 text-slate-800 h-10 w-10 sm:w-16"
                                 }
+                                handler={() => handleClick(2)}
                             />
                             <Button
                                 text="3"
                                 design={
                                     "bg-gray-200 border-b-gray-400 text-slate-800 h-10 w-10 sm:w-16"
                                 }
+                                handler={() => handleClick(3)}
                             />
                             <Button
                                 text="-"
                                 design={
                                     "bg-gray-200 border-b-gray-400 text-slate-800 h-10 w-10 sm:w-16"
                                 }
+                                handler={() => handleClick("-")}
                             />
                         </div>
                         <div className="flex justify-around mb-2">
@@ -100,24 +160,28 @@ export const App = () => {
                                 design={
                                     "bg-gray-200 border-b-gray-400 text-slate-800 h-10 w-10 sm:w-16"
                                 }
+                                handler={() => handleClick(".")}
                             />
                             <Button
                                 text="0"
                                 design={
                                     "bg-gray-200 border-b-gray-400 text-slate-800 h-10 w-10 sm:w-16"
                                 }
+                                handler={() => handleClick(0)}
                             />
                             <Button
                                 text="/"
                                 design={
                                     "bg-gray-200 border-b-gray-400 text-slate-800 h-10 w-10 sm:w-16"
                                 }
+                                handler={() => handleClick("/")}
                             />
                             <Button
                                 text="x"
                                 design={
                                     "bg-gray-200 border-b-gray-400 text-slate-800 h-10 w-10 sm:w-16"
                                 }
+                                handler={() => handleClick("x")}
                             />
                         </div>
                         <div className="flex justify-around mb-2">
@@ -126,18 +190,19 @@ export const App = () => {
                                 design={
                                     "h-10 w-28 sm:w-36 bg-indigo-500 border-b-indigo-700 text-white text-xl"
                                 }
+                                handler={() => removeAll()}
                             />
                             <Button
                                 text="="
                                 design={
                                     "bg-rose-500 h-10 w-28 sm:w-36 border-b-rose-700 text-white text-xl"
                                 }
+                                handler={() => handleResult()}
                             />
                         </div>
                     </div>
                 </div>
             </div>
-            <Footer />
         </>
     );
 };
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,10 @@
-export const Button = ({ text, design }) => {
+export const Button = ({text, design, handler}) => {
     //console.log(text);
     return (
         <button
             className={`rounded-lg p-7 flex items-center justify-center border-b-4 cursor-pointer text-slate-800 text-2xl font-bold ${design}`}
             value={text}
+            onClick={()=> handler(text)}
         >
             {text}
         </button>
